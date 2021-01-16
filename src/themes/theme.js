1c6944ac85307c799a83c94a1e3dddb9927ec826
Jiannine Torres <jiannine@agilebits.com>
2021-01-16 14:52:03 -0500

    Create theme.js

---
+++ src/themes/theme.js
@@ -0,0 +1,23 @@
+export default {
+  fonts: {
+    main: "Montserrat, sans-serif",
+    code: "Roboto Mono, monospace",
+  },
+  colors: {
+    black: "#212F3D",
+    blue: "#2F80ED",
+    white: "#fff",
+  },
+  breakpoints: {
+    mobile: "only screen and (min-width: 425px)",
+    tabletPortrait: "only screen and (min-width: 768px)",
+    tabletLandscape: "only screen and (min-width: 1024px)",
+    desktop: "only screen and (min-width: 1200px)",
+    desktopL: "only screen and (min-width: 1440px)",
+    desktopXL: "only screen and (min-width: 1600px)",
+    desktopXXL: "only screen and (min-width: 1920px)",
+  },
+  spacings: {
+    base: "16px",
+  },
+};
