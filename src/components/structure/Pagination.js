8bbfacdc8e4f47256c1a7682b9b006c7105b2c62
Jiannine Torres <jiannine@agilebits.com>
2021-02-01 19:04:59 -0500

    Rename isfirst and islast to isfirstBlogPage and islastBlogPage

--- src/components/structure/Pagination.js
+++ src/components/structure/Pagination.js
@@ -17,28 +17,38 @@ const PaginationStyles = styled((props) => <Link {...props} />)`
   // Previous page
   &:nth-child(1) {
     color: ${(props) =>
-      props.isfirst ? props.theme.colors.dark3 : props.theme.colors.dark1};
-    pointer-events: ${(props) => (props.isfirst ? "none" : "auto")};
-    cursor: ${(props) => (props.isfirst ? "default" : "pointer")};
+      props.isfirstBlogPage
+        ? props.theme.colors.dark3
+        : props.theme.colors.dark1};
+    pointer-events: ${(props) => (props.isfirstBlogPage ? "none" : "auto")};
+    cursor: ${(props) => (props.isfirstBlogPage ? "default" : "pointer")};
   }
 
   // Next page
   &:nth-child(3) {
     color: ${(props) =>
-      props.islast ? props.theme.colors.dark3 : props.theme.colors.dark1};
-    pointer-events: ${(props) => (props.islast ? "none" : "auto")};
-    cursor: ${(props) => (props.islast ? "default" : "pointer")};
+      props.islastBlogPage
+        ? props.theme.colors.dark3
+        : props.theme.colors.dark1};
+    pointer-events: ${(props) => (props.islastBlogPage ? "none" : "auto")};
+    cursor: ${(props) => (props.islastBlogPage ? "default" : "pointer")};
   }
 `;
 
-const Pagination = ({ prevPage, nextPage, homePage, isfirst, islast }) => {
+const Pagination = ({
+  prevPage,
+  nextPage,
+  homePage,
+  isfirstBlogPage,
+  islastBlogPage,
+}) => {
   return (
     <PaginationWrapper>
-      <PaginationStyles isfirst={isfirst} to={prevPage}>
+      <PaginationStyles isfirstBlogPage={isfirstBlogPage} to={prevPage}>
         Previous page
       </PaginationStyles>
       <PaginationStyles to={homePage}>Home</PaginationStyles>
-      <PaginationStyles islast={islast} to={nextPage}>
+      <PaginationStyles islastBlogPage={islastBlogPage} to={nextPage}>
         Next page
       </PaginationStyles>
     </PaginationWrapper>
