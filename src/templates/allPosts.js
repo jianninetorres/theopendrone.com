ca6aa5d02140db86c862fbc4b35091d94ececa7a
Jiannine Torres <jiannine@agilebits.com>
2021-02-01 19:04:27 -0500

    Create paginated pages programatically

---
+++ src/templates/allPosts.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { graphql } from "gatsby";
+import Pagination from "../components/structure/Pagination";
+import styled from "styled-components";
+
+const PostStyles = styled.div`
+  position: relative;
+  top: 100px;
+`;
+
+const allPosts = ({ pageContext, data }) => {
+  const { currentPage, numPages } = pageContext;
+  const homePage = `/`;
+  const isfirstBlogPage = currentPage === 1 ? true : false;
+  const islastBlogPage = currentPage === numPages ? true : false;
+  const prevPage = currentPage - 1 === 0 ? "" : `/blog/${currentPage - 1}/`;
+
+  console.log("currentPage", currentPage);
+  console.log(`isfirstBlogPage, ${isfirstBlogPage}`);
+  console.log("currentPage - 1:", currentPage - 1);
+  console.log("prevPage:", prevPage);
+
+  // give nextPage a route if there is a page following the current page
+  const nextPage =
+    currentPage + 1 === numPages ? `/blog/${currentPage + 1}` : "";
+  const posts = data.allMdx.edges;
+
+  return (
+    <PostStyles>
+      {posts.map((post) => (
+        <div
+          key={post.node.frontmatter.slug}
+          date={post.node.frontmatter.date}
+          title={post.node.frontmatter.title}
+          excerpt={post.node.frontmatter.excerpt}
+          slug={post.node.frontmatter.slug}
+        />
+      ))}
+      <Pagination
+        homePage={homePage}
+        isfirstBlogPage={isfirstBlogPage}
+        islastBlogPage={islastBlogPage}
+        prevPage={prevPage}
+        nextPage={nextPage}
+      />
+    </PostStyles>
+  );
+};
+
+export default allPosts;
+
+export const pageQuery = graphql`
+  query AllPostsQuery($skip: Int!, $limit: Int!) {
+    allMdx(
+      sort: { fields: frontmatter___date, order: DESC }
+      skip: $skip
+      limit: $limit
+    ) {
+      edges {
+        node {
+          frontmatter {
+            slug
+            title
+            date(formatString: "MMMM DD, YYYY")
+            excerpt
+          }
+        }
+      }
+    }
+  }
+`;
