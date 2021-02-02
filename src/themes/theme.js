0501f1bac8fd601e89a70b9e3ec89ffdbb428f4b
Jiannine Torres <jiannine@agilebits.com>
2021-02-01 19:01:01 -0500

    Add temporary colours

--- src/themes/theme.js
+++ src/themes/theme.js
@@ -7,6 +7,13 @@ export default {
     black: "#212F3D",
     blue: "#2F80ED",
     white: "#fff",
+    main1: "hsl(207,70%,59%)",
+    main2: "hsl(207,70%,94%)",
+    dark1: "hsl(227,2%,12%)",
+    dark2: "hsl(227,2%,26%)",
+    dark3: "hsl(227,2%,64%)",
+    light1: "hsl(0,0%,97%)",
+    light2: "hsl(0,0%,99%)",
   },
   breakpoints: {
     mobile: "only screen and (min-width: 425px)",
