605c55e3cdc9b48b59ffe2c81ae4e3ce76585573
Jiannine Torres <jiannine@agilebits.com>
2021-02-11 18:58:32 -0500

    Add fontawesome

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     // "gatsby-plugin-sitemap",
     "gatsby-plugin-offline",
     "gatsby-transformer-sharp",
+    "gatsby-plugin-fontawesome-css",
     {
       resolve: `gatsby-plugin-mdx`,
       options: {
