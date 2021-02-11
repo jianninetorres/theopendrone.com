fa8f46e450da12795b9ddfd062af18f5f7b29ffb
Jiannine Torres <jiannine@agilebits.com>
2021-02-11 18:59:22 -0500

    Add more styles and props to ContentP component

--- src/components/structure/ContentP.js
+++ src/components/structure/ContentP.js
@@ -3,16 +3,42 @@ import styled from "styled-components";
 
 const ContentPStyles = styled.p`
   width: 100%;
+  color: ${(props) => props.textColor || props.theme.colors.black};
   max-width: ${(props) => props.maxWidth || ""};
   font-size: 1rem;
+  margin: ${(props) => props.margins || ""};
+  margin-top: ${(props) => props.marginTop || ""};
+  margin-bottom: ${(props) => props.marginBottom || props.theme.spacings.base};
+  text-align: ${(props) => props.textAlign || "left"};
 
   @media ${(props) => props.theme.breakpoints.tabletPortrait} {
-    font-size: 1.25rem;
+    font-size: ${(props) => props.fontSize || "1.25rem"};
   }
 `;
 
-const ContentP = ({ children, maxWidth }) => {
-  return <ContentPStyles maxWidth={maxWidth}>{children}</ContentPStyles>;
+const ContentP = ({
+  children,
+  maxWidth,
+  fontSize,
+  margins,
+  marginTop,
+  marginBottom,
+  textAlign,
+  textColor,
+}) => {
+  return (
+    <ContentPStyles
+      maxWidth={maxWidth}
+      fontSize={fontSize}
+      margins={margins}
+      marginTop={marginTop}
+      marginBottom={marginBottom}
+      textAlign={textAlign}
+      textColor={textColor}
+    >
+      {children}
+    </ContentPStyles>
+  );
 };
 
 export default ContentP;
