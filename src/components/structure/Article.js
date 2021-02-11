69fa4a856f869cab1c1614bfb61556135eab2eaa
Jiannine Torres <jiannine@agilebits.com>
2021-02-11 18:59:06 -0500

    Add more styles and props to Article component

--- src/components/structure/Article.js
+++ src/components/structure/Article.js
@@ -1,7 +1,32 @@
 import React from "react";
+import styled from "styled-components";
 
-const Article = ({ children }) => {
-  return <article>{children}</article>;
+const ArticleStyles = styled.article`
+  background-color: ${(props) => props.bgColor || props.theme.colors.white};
+  padding: calc(var(--base-size) * 2);
+  display: flex;
+  flex-direction: ${(props) => props.flexDirection || "row"};
+  justify-content: ${(props) => props.justifyContent || "flex-start"};
+  align-items: ${(props) => props.alignItems || "flex-start"};
+`;
+
+const Article = ({
+  children,
+  bgColor,
+  flexDirection,
+  justifyContent,
+  alignItems,
+}) => {
+  return (
+    <ArticleStyles
+      bgColor={bgColor}
+      flexDirection={flexDirection}
+      justifyContent={justifyContent}
+      alignItems={alignItems}
+    >
+      {children}
+    </ArticleStyles>
+  );
 };
 
 export default Article;
