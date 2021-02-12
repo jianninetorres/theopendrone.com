cf23b809851b6bd3b4f5af4cbd50b981b9321c39
Jiannine Torres <jiannine@agilebits.com>
2021-02-12 17:48:22 -0500

    Start Footer component setup

---
+++ src/components/structure/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const FooterStyles = styled.footer``;
+
+const Footer = () => {
+  return <FooterStyles></FooterStyles>;
+};
+
+export default Footer;
