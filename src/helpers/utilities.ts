7c3ec4c8d2d172f10862a6d58bee23cbbe26a7b1
Jiannine Torres <jiannine@agilebits.com>
2021-02-13 10:18:24 -0500

    Create back to previous page helper

--- src/helpers/utilities.ts
+++ src/helpers/utilities.ts
@@ -5,3 +5,7 @@ export const mediaQuery = (minWidth) => {
 export const windowOffSetY = () => {
   return window.pageYOffset || document.documentElement.scrollTop;
 };
+
+export const backToPreviousPage = () => {
+  return window.history.go(-1);
+};
