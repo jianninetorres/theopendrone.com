ce22afc8d9ad1257cb33e50028a23af8717435f7
Jiannine Torres <jiannine@agilebits.com>
2021-02-14 14:39:00 -0500

    Add link back to all updates

--- src/templates/singlePost.js
+++ src/templates/singlePost.js
@@ -38,6 +38,7 @@ const singlePost = ({ data, pageContext }) => {
             </Link>
           )}
         </div>
+        <Link to="/updates">All updates</Link>
         <div>
           {olderPost && (
             <Link
