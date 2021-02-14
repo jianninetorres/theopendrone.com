438f591706810e6f845f714f1898641cf36b36b8
Jiannine Torres <jiannine@agilebits.com>
2021-02-13 22:21:50 -0500

    Add styles to Post pagination

--- src/components/structure/Post.js
+++ src/components/structure/Post.js
@@ -1,8 +1,6 @@
 import React from "react";
-import { backToPreviousPage } from "../../helpers/utilities";
 import styled from "styled-components";
 import Article from "./Article";
-import Button from "./Button";
 
 const PostStyles = styled.div`
   position: relative;
@@ -16,19 +14,50 @@ const PostStyles = styled.div`
   p {
     margin-bottom: calc(var(--base-size) * 2);
   }
-`;
 
-const backToBlog = () => {
-  backToPreviousPage();
-};
+  .post-pagination-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+
+    @media ${(props) => props.theme.breakpoints.tabletLandscape} {
+      flex-direction: row;
+      justify-content: space-between;
+    }
+
+    a {
+      display: inline-flex;
+      color: ${(props) => props.theme.colors.black};
+      text-decoration: none;
+      width: fit-content;
+      margin: var(--base-size) 0;
+      &:hover {
+        color: ${(props) => props.theme.colors.blue};
+        > svg {
+          color: ${(props) => props.theme.colors.blue};
+        }
+      }
+
+      &.newer {
+        @media ${(props) => props.theme.breakpoints.tabletLandscape} {
+          place-self: flex-start;
+        }
+      }
+
+      &.older {
+        @media ${(props) => props.theme.breakpoints.tabletLandscape} {
+          place-self: flex-end;
+        }
+      }
+    }
+  }
+`;
 
 const Post = ({ children }) => {
   return (
     <PostStyles>
-      <Article flexDirection="column">
-        {children}
-        <Button onClick={backToBlog}>Back to blog</Button>
-      </Article>
+      <Article flexDirection="column">{children}</Article>
     </PostStyles>
   );
 };
